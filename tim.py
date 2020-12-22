9deef20bc73c7d6fe2b5fc3301ee92b47dd0159d
ToyodaMayu <mny.8-hunny1@docomo.ne.jp>
2020-12-22 15:51:21 +0900

    枚数と秒数の受け渡し可能、桁数5に指定

--- tim.py
+++ tim.py
@@ -10,33 +10,30 @@ def save_all_frames(video_path, dir_path, basename, ext='jpg'):
     os.makedirs(dir_path, exist_ok=True)
     base_path = os.path.join(dir_path, basename)
 
-    digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
     fps = cap.get(cv2.CAP_PROP_FPS)
     frame_num = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     play_time = frame_num / fps
     
     global n
-    global num
+    global ttime
     n=0
     while True:
         ret, frame = cap.read()
         #frame=cv2.rotate(frame,cv2.ROTATE_90_COUNTERCLOCKWISE)
         frame=cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
         if ret:
-            cv2.imwrite('{}_{}.{}'.format(base_path, str(n).zfill(digit), ext), frame)
+            cv2.imwrite('{}_{}.{}'.format(base_path, str(n).zfill(5), ext), frame)
             n += 1 #nは画像になった枚数
         else:
             time=play_time/n
             ttime='{:.3f}'.format(time)
-            nu=str(n)
-            num=len(nu) #画像枚数が何桁か
             break
 
 def count():
     return n
 
-def cou():
-    return num
+def second():
+    return ttime
 
 
 save_all_frames('sample.MOV', 'sample-photo', 'sample', 'png')
